277816d20d1ae8f5918e8257535fd2cb5398def7
Iglesias-Leafwind <pedrohpi@ua.pt>
2021-04-26 21:16:00 +0100

    OCR function in processing.py half implemented (NLP to do)

--- project/app/processing.py
+++ project/app/processing.py
@@ -15,6 +15,9 @@ from torch.nn import functional as F
 import os
 from PIL import Image
 
+from imutils.object_detection import non_max_suppression
+import cv2
+import pytesseract
 features = []
 imageFeatures = []
 
@@ -120,8 +123,139 @@ def getPlaces(img_name):
 
     return classes[idx[0]]
 
+def getOCR(img_path):
+        #load installed tesseract-ocr from users pc
+    pytesseract.pytesseract.tesseract_cmd = r'D:\Programs\Tesseract-OCR\tesseract'
+    custom_config = r'--oem 3 --psm 6'
+    east = "../frozen_east_text_detection.pb"
+    min_confidence = 0.6
 
+    results = []
+    #These must be multiple of 32
+    newW = 128
+    newH = 128
+    net = cv2.dnn.readNet(east)
+    image = cv2.imread(image_path)
+    orig = image.copy()
+    (H, W) = image.shape[:2]
+    
+    # set the new width and height and then determine the ratio in change
+    # for both the width and height
+    rW = W / float(newW)
+    rH = H / float(newH)
+    
+    # resize the image and grab the new image dimensions
+    image = cv2.resize(image, (newW, newH))
+    (H, W) = image.shape[:2]
+    
+    # define the two output layer names for the EAST detector model that
+    # we are interested -- the first is the output probabilities and the
+    # second can be used to derive the bounding box coordinates of text
+    layerNames = [
+            "feature_fusion/Conv_7/Sigmoid",
+            "feature_fusion/concat_3"]
+    
+    # construct a blob from the image and then perform a forward pass of
+    # the model to obtain the two output layer sets
+    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+            (123.68, 116.78, 103.94), swapRB=True, crop=False)
 
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+
+    # grab the number of rows and columns from the scores volume, then
+    # initialize our set of bounding box rectangles and corresponding
+    # confidence scores
+    (numRows, numCols) = scores.shape[2:4]
+    rects = set()
+    confidences = []
+    # loop over the number of rows
+    for y in range(0, numRows):
+            # extract the scores (probabilities), followed by the geometrical
+            # data used to derive potential bounding box coordinates that
+            # surround text
+            scoresData = scores[0, 0, y]
+            xData0 = geometry[0, 0, y]
+            xData1 = geometry[0, 1, y]
+            xData2 = geometry[0, 2, y]
+            xData3 = geometry[0, 3, y]
+            anglesData = geometry[0, 4, y]
+            # loop over the number of columns
+            for x in range(0, numCols):
+                    # if our score does not have sufficient probability, ignore it
+                    if scoresData[x] < min_confidence:
+                            continue
+                    # compute the offset factor as our resulting feature maps will
+                    # be 4x smaller than the input image
+                    (offsetX, offsetY) = (x * 4.0, y * 4.0)
+                    # extract the rotation angle for the prediction and then
+                    # compute the sin and cosine
+                    angle = anglesData[x]
+                    cos = np.cos(angle)
+                    sin = np.sin(angle)
+                    # use the geometry volume to derive the width and height of
+                    # the bounding box
+                    h = xData0[x] + xData2[x]
+                    w = xData1[x] + xData3[x]
+                    # compute both the starting and ending (x, y)-coordinates for
+                    # the text prediction bounding box
+                    endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+                    endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+                    startX = int(endX - w)
+                    startY = int(endY - h)
+                    # add the bounding box coordinates and probability score to
+                    # our respective lists
+                    rects.append((startX, startY, endX, endY))
+                    confidences.append(scoresData[x])
+    # apply non-maxima suppression to suppress weak, overlapping bounding
+    # boxes
+    boxes = non_max_suppression(np.array(rects), probs=confidences)
+    # loop over the bounding boxes
+    (maxH, maxW) = orig.shape[:2]
+    for (startX, startY, endX, endY) in boxes:
+            # scale the bounding box coordinates based on the respective
+            # ratios
+            startX = int(startX * rW) - 20
+            if(startX <= 0):
+                startX = 1
+            startY = int(startY * rH) - 20
+            if(startY <= 0):
+                startY = 1
+            endX = int(endX * rW) + 20
+            if(endX >= maxW):
+                endX = maxW-1
+            endY = int(endY * rH) + 20
+            if(endY >= maxH):
+                endY = maxH-1
+            # draw the bounding box on the image
+            ROI = orig[startY:endY, startX:endX]
+            imageText = pytesseract.image_to_string(ROI, config=custom_config)
+            results.add(imageText.replace("\x0c"," ").replace("\n"," "))
+  
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Load image, grayscale, Gaussian blur, adaptive threshold
+    gray = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (9,9), 0)
+    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,30)
+
+    # Dilate to combine adjacent text contours
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+    dilate = cv2.dilate(thresh, kernel, iterations=4)
+
+    # Find contours, highlight text areas, and extract ROIs
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area > 10000:
+            x,y,w,h = cv2.boundingRect(c)
+            ROI = orig[y:y+h, x:x+w]
+            imageText = pytesseract.image_to_string(ROI, config=custom_config)
+            results.add(imageText.replace("\x0c"," ").replace("\n"," "))
+    print(results)
+
+    
 # load all images to memory
 def setUp():
     images = ImageNeo.nodes.all()
